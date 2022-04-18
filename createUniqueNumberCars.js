41578f6651e2451746e2b398db386f84fa5a070c
Ganenco Dmitri <ganenko.dmitrii@gmail.com>
2022-04-18 22:00:55 +0300

    Added initial files

---
+++ createUniqueNumberCars.js
@@ -0,0 +1,37 @@
+// const englishLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+// const numberList = "0123456789".split("");
+
+const addNumberCar = (letters, numbers) => {
+  let carNumber = "";
+  const carsNumbers = [];
+
+  for (let x = 0; x < letters.length; x++) {
+    for (let j = 0; j < letters.length; j++) {
+      if (letters[x] !== letters[j]) {
+        for (let e = 0; e < numbers.length; e++) {
+          for (let w = 0; w < numbers.length; w++) {
+            for (let z = 0; z < numbers.length; z++) {
+              if (
+                numbers[e] !== numbers[w] &&
+                numbers[e] !== numbers[z] &&
+                numbers[z] !== numbers[w]
+              ) {
+                carNumber =
+                  letters[x] +
+                  letters[j] +
+                  numbers[e] +
+                  numbers[w] +
+                  numbers[z];
+                carsNumbers.push(carNumber);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return carsNumbers;
+};
+
+module.exports = addNumberCar;
